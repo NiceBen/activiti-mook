6d564619b5495c04515c19145a95877884494df4
OfBen <jpzhou2017@126.com>
2021-12-06 18:30:25 +0800

    更改了打包方式：由jar修改为war

--- src/main/java/com/deo/activitimook/ActivitiMookApplication.java
+++ src/main/java/com/deo/activitimook/ActivitiMookApplication.java
@@ -2,9 +2,21 @@ package com.deo.activitimook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ActivitiMookApplication {
+public class ActivitiMookApplication extends SpringBootServletInitializer {
+
+    /**
+     * 将 Springboot 的 jar 包发布方式修改为 war 包发布方式
+     * @param builder
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ActivitiMookApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ActivitiMookApplication.class, args);
