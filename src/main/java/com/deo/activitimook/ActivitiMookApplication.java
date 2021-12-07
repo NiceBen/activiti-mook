6c00d1de9861a0033f45d52bc6ac1ad08fdb63ca
OfBen <jpzhou2017@126.com>
2021-12-07 15:01:52 +0800

    add deployment config

--- src/main/java/com/deo/activitimook/ActivitiMookApplication.java
+++ src/main/java/com/deo/activitimook/ActivitiMookApplication.java
@@ -1,25 +1,37 @@
 package com.deo.activitimook;
 
+// jar 包发布方式
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ActivitiMookApplication extends SpringBootServletInitializer {
-
-    /**
-     * 将 Springboot 的 jar 包发布方式修改为 war 包发布方式
-     * @param builder
-     * @return
-     */
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(ActivitiMookApplication.class);
-    }
+public class ActivitiMookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ActivitiMookApplication.class, args);
     }
-
 }
+
+// // war 包发布方式
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//@SpringBootApplication
+//public class ActivitiMookApplication extends SpringBootServletInitializer {
+//
+//    /**
+//     * 将 Springboot 的 jar 包发布方式修改为 war 包发布方式
+//     * @param builder
+//     * @return
+//     */
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(ActivitiMookApplication.class);
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ActivitiMookApplication.class, args);
+//    }
+//}
